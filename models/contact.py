0a020283207b25896bb52959798211cab0457ba3
DonaChris <donachristian.agossou@gmail.com>
2026-01-13 17:40:20 +0100

    feat: suppression de __init__.py dans database/ et ajout du model Contact

---
+++ models/contact.py
@@ -0,0 +1,12 @@
+""" DEFINIT LES DIFFERENTES DONNEES D'UN CONTACT """
+
+class Contact:
+    def __init__(self, id, nom, prenom, telephone, email):
+        self.id = id
+        self.nom = nom
+        self.prenom = prenom
+        self.telephone = telephone
+        self.email = email
+
+    def __str__(self):
+        return f"Nom: {self.nom}, Prénom: {self.prenom}, Téléphone: {self.telephone}, Email: {self.email}"
